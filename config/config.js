d8b1c57be1a25ce2e2cd4a6e8f3b500bcd873a60
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-09 19:47:47 +0300

    db posgres set up

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
-const { Client } = require('pg');
+const { Client }= require('pg');
 const env = require('dotenv').config();
 
 const config = new Client({
-  host: 'localhost',
-  port: '5432',
-  user: 'postgres',
-  password: 'ingurat1991',
-  database: 'rects_api'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 module.exports = config;
