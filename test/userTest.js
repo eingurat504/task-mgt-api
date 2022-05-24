9fb03f70d91cd9b4ed072ea4c8e1d48c742e4449
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-24 23:22:30 +0300

    user test updates

--- test/userTest.js
+++ test/userTest.js
@@ -2,23 +2,52 @@ const assert = require('assert');
 const db = require("../models");
 const User = db.users;
 
-// beforeEach(async function () {
-//     await db.clear();
-//     await db.save([tobi, loki, jane]);
-//   });
-  
-  describe('#find()', function () {
-    it('responds with matching records', async function () {
-      const users = await db.findAll({type: 'User'});
-      users.should.have.length(3);
+
+  // describe('#find()', function () {
+  //   it('responds with matching records', async function () {
+  //     const users = await db.findAll({type: 'User'});
+  //     users.should.have.length(3);
+  //   });
+  // });
+
+  // describe('User', function () {
+  //   describe('#save()', function () {
+  //     it('should save without error', function (done) {
+  //       var user = new User('Luna');
+  //       user.save();
+  //     });
+  //   });
+  // });
+
+describe('api', function () {
+   
+  describe('GET /api/users getUsers', function () {
+    it('respond with an array of users', function () {
+      const users = User.findAll();
+      assert.ok(true);
     });
   });
 
-  describe('User', function () {
-    describe('#save()', function () {
-      it('should save without error', function (done) {
-        var user = new User('Luna');
-        user.save();
-      });
+  describe('GET /api/users/deactivated deactivatedUsers', function () {
+    it('respond with an array of deactivated users', function () {
+      const users = User.findAll();
+      assert.ok(true);
     });
   });
+
+  describe('GET /api/users/active getActiveUsers', function () {
+    it('respond with an array of active users', function () {
+      const users = User.findAll();
+      assert.ok(true);
+    });
+  });
+
+  describe('GET /api/users/me getProfile', function () {
+    it('respond with an array of user profile', function () {
+      const users = User.findAll();
+      assert.ok(true);
+    });
+  });
+
+});
+  
