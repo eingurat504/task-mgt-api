0fc0a8efaeba9129f7d0ad5120a39972787b56f0
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-24 23:42:32 +0300

    documentation

--- test/projectTest.js
+++ test/projectTest.js
@@ -2,23 +2,53 @@ const assert = require('assert');
 const db = require("../models");
 const Project = db.projects;
 
-// beforeEach(async function () {
-//     await db.clear();
-//     await db.save([tobi, loki, jane]);
-//   });
-  
-  describe('#find()', function () {
-    it('responds with matching records', async function () {
-      const projects = await Project.findAll({type: 'Project'});
-      projects.should.have.length(3);
+  // describe('#find()', function () {
+  //   it('responds with matching records', async function () {
+  //     const projects = await Project.findAll({type: 'Project'});
+  //     projects.should.have.length(3);
+  //   });
+  // });
+
+  // describe('Project', function () {
+  //   describe('#save()', function () {
+  //     it('should save without error', function (done) {
+  //       var project = new Project('Luna');
+  //       project.save();
+  //     });
+  //   });
+  // });
+
+describe('api', function () {
+   
+  /**
+   * All Projects
+   */
+  describe('GET /api/projects getProjects', function () {
+    it('respond with an array of projects', function () {
+      const projects = Project.findAll();
+      assert.ok(true);
     });
   });
 
-  describe('Project', function () {
-    describe('#save()', function () {
-      it('should save without error', function (done) {
-        var project = new Project('Luna');
-        project.save();
-      });
+  /**
+   * All Pending Projects
+   */
+  describe('GET /api/projects/pending pendingProjects', function () {
+    it('respond with an array of pending projects', function () {
+      const projects = Project.findAll();
+      assert.ok(true);
     });
   });
+
+  /**
+   * All Completed Projects
+   */
+  describe('GET /api/projects/completed getcompletedProjects', function () {
+    it('respond with an array of completed users', function () {
+      const projects = Project.findAll();
+      assert.ok(true);
+    });
+  });
+
+});
+  
